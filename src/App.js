b92b2644d0a71c8c64774d61b498bb7fc781f5af
Jose Miguel Garcia <mike80@MacBook-Pro-de-Jose.local>
2022-11-07 12:36:05 -0600

    Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Layout from './components/Layout';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Layout>
+      <Login />
+    </Layout>
+  );
+}
+
+export default App;
